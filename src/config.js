960dcce53cf13db06912ec39b34fd4c0ea06b96a
dsfalkjfdadlkf <wtacazx@gmail.com>
2026-03-30 19:58:57 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const app_config = {
   client_id: "1488185208493576274",
   client_secret: "Lv-1OXH9WgaUZB161xJiUcs-uh5Yp--Z",
-  supa_url: "https://jkzhzdopyvyhzvdomqzx.supabase.co",
+  supa_url: "https://jkzhzdopyvyhzvdomqzx.supabase.co/auth/v1/callback",
   supa_anon_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impremh6ZG9weXZ5aHp2ZG9tcXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NTgyMzQsImV4cCI6MjA5MDQzNDIzNH0.Z-DZKNanAJ9g_E-8qCfIfa8XanQ1Iam9qJlfrmFnIfY",
   supa_service_role: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impremh6ZG9weXZ5aHp2ZG9tcXp4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDg1ODIzNCwiZXhwIjoyMDkwNDM0MjM0fQ.vWcWYoCFdsBOxM4fOWY0eynpUE4qlMaVR5Rr9GMfUyQ",
-  redirect_uri: "https://expose-center.vercel.app/callback",
+  redirect_uri: "https://jkzhzdopyvyhzvdomqzx.supabase.co/auth/v1/callback",
   owner_ids: ["899263378143473695", "1435621020001108069"],
   server_id: "1487863855798288467",
   default_types: ["pedophile", "skid", "doxxer", "scammer", "other"],
